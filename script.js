598e2d793bfa66ddc673baf9e4034075c889c55a
Shahu Patil <139709118+Shahu2002@users.noreply.github.com>
2023-09-17 23:03:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // script.js
 
 function getFormvalue() {
-	 alert(`${firstName} ${lastName}`);
+    //Write your code here
+    const form = document.getElementById("form1");
+
+    const firstName = form.elements.fname.value;
+    const lastName = form.elements.lname.value;
+
+    alert(`${firstName} ${lastName}`);
 }
 
