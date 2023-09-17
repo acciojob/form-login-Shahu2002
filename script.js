14592c0583e1b66599445e87f1925949fea4e2d5
Shahu Patil <139709118+Shahu2002@users.noreply.github.com>
2023-09-17 22:48:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 // script.js
 
+// script.js
+
 function getFormvalue() {
-  // Get the form element by its ID
-  var form = document.getElementById("myForm");
-
-  // Access the input elements for first and last name by their IDs
-  var firstNameInput = document.getElementById("firstName");
-  var lastNameInput = document.getElementById("lastName");
-
-  // Get the values entered by the user
-  var firstName = firstNameInput.value;
-  var lastName = lastNameInput.value;
+  // Get the value of the first name and last name input fields
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
 
   // Check if either first name or last name is empty
   if (firstName === "" || lastName === "") {
@@ -18,10 +13,6 @@ function getFormvalue() {
   } else {
     // Display the first and last name using an alert
     alert("First Name: " + firstName + "\nLast Name: " + lastName);
-
-    // You can also clear the form fields if needed
-    firstNameInput.value = "";
-    lastNameInput.value = "";
   }
 
   // Prevent the form from submitting and refreshing the page
