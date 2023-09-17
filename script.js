776cfb8cc42c0a6ab45cda1e05d471c38c4d62bf
Shahu Patil <139709118+Shahu2002@users.noreply.github.com>
2023-09-17 22:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 // script.js
 
-// script.js
-
 function getFormvalue() {
-  // Get the value of the first name and last name input fields
-  var firstName = document.getElementById("firstName").value;
-  var lastName = document.getElementById("lastName").value;
-
-  // Check if either first name or last name is empty
-  if (firstName === "" || lastName === "") {
-    alert("Please enter both first and last names.");
-  } else {
-    // Display the first and last name using an alert
-    alert("First Name: " + firstName + "\nLast Name: " + lastName);
-  }
-
-  // Prevent the form from submitting and refreshing the page
-  return false;
+	 alert();
 }
 
